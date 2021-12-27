0d6bf65432cbf64e02a8d9a4d6fb32df1f4d7991
William Trépanier <wtrepanier@coveo.com>
2021-12-27 18:25:56 -0500

    added flame container

--- filter_plugins/plugins.py
+++ filter_plugins/plugins.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python
 
 def extract_secrets_from_services(services):
-    assert isinstance(services, dict)
-
     merged_secrets = []
     for service in services.values():
         secrets = service.get("secrets", [])
-        assert isinstance(secrets, list)
         merged_secrets += secrets
-
     return merged_secrets
+
+
+def parse_uid_and_gid(output):
+    return {result["name"]: {"uid": result["uid"], "gid": result["group"]} for result in output["results"]}
 
 
 class FilterModule(object):
     filter_map = {
         'extract_secrets_from_services': extract_secrets_from_services,
+        'parse_uid_and_gid': parse_uid_and_gid,
     }
 
     def filters(self):
